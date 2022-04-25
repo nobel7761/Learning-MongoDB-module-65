b07070231df22a461f54a3de40c090feb884e655
nobel7761 <habiburnobel@gmail.com>
2022-04-26 04:29:41 +0600

    module 65-4

---
+++ react-node-mongodb/src/components/AddUser/AddUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h2>Please Add a New User</h2>
+        </div>
+    );
+};
+
+export default AddUser;
